e7668eb7d7ef04abb867cbef8564033717cfb2ee
priyampinju <pinjupriyam1826@gmail.com>
2024-02-03 22:45:05 +0530

    all done

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 
 yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'Yeeeiii finally 🤩🎉 So when are we meeting?';
+    question.innerHTML = 'Yeeeiii finally 🤩🎉';
     yesBtn.style = 'display:none'
     noBtn.style = 'display:none'
     refBtn.style = 'display:inline'
